74325d3bfb4ac56eaba709e39e10441f7d72ff1f
Empower <Empower@DESKTOP-HO66STI>
2023-03-28 14:08:17 +0200

    Add project files.

---
+++ Empower_hsd1_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Empower_hsd1_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Empower_hsd1_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("71437546-2906-40BD-BCEA-2F941B6F8746")]
+[assembly: AssemblyVersion("1.0.0.0")]
